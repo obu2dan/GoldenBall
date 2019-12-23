1bd2dbb18e2e322b0d769d88bc43e2feda575047
佐藤陸 <satouriku@satourikunoMacBook-Pro.local>
2019-12-23 15:22:24 +0900

    タイトルシーンに効果音をつけました。

--- Assets/Scripts/Title.cs
+++ Assets/Scripts/Title.cs
@@ -5,17 +5,28 @@ using UnityEngine.SceneManagement;
 
 public class Title : MonoBehaviour
 {
+    private AudioSource title_sound;
+    [SerializeField]
+    private AudioClip title_se;
+    private bool is_se = true;
     // Start is called before the first frame update
     void Start()
     {
-        
+        title_sound = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.Space)){
-            SceneManager.LoadScene(1);
+        if(Input.GetKeyDown(KeyCode.Space) && is_se){
+            title_sound.PlayOneShot(title_se);
+            is_se = false;
+            StartCoroutine(TitleScene(0.5f));
         }
+    }
+    IEnumerator TitleScene(float wait_time)
+    {
+        yield return new WaitForSeconds(wait_time);
+        SceneManager.LoadScene(1);
     }
 }
